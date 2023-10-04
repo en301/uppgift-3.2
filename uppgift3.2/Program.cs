3d1c1524a30035fa7d8b4d8e815d592defe8574e
emsj06001 <emsj06001@E-PW024NWP>
2023-10-04 10:39:43 +0200

    Add project files.

---
+++ uppgift3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgift_3_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? Svara j för ja och n för nej...");
+            string gymnasie = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            string siffra = Console.ReadLine();
+            int ålder = int.Parse(siffra);
+
+            if (ålder < 22 && gymnasie == "j")
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter andra");
+            }
+            Console.ReadKey();
+        }
+    }
+}
